ab9cd3d24b8213460b868544e0254bcb8971e6ec
Tola Awofolu <agapebondservant@yahoo.com>
2026-09-15 17:35:28 -0400

    Sync

--- workflows/examples/code_understanding/telemetry/mlflow_custom_telemetry.py
+++ workflows/examples/code_understanding/telemetry/mlflow_custom_telemetry.py
@@ -11,5 +11,5 @@ class MlFlowCustomTelemetry(CustomTelemetry):
         tracking_uri = os.environ.get("MLFLOW_TRACKING_URI")
         if tracking_uri:
             mlflow.set_tracking_uri(tracking_uri)
-            litellm.callbacks = ["mlflow"]
+        litellm.callbacks = ["mlflow"]
 
